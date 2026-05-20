665e6fd60d827dfbad36329d086729cfd191a069
navithaananthula <navithaanantula@gmail.com>
2026-05-20 11:41:46 +0530

    added example2.py

---
+++ example2.py
@@ -0,0 +1,14 @@
+print("Employee Details")
+
+name = "Navitha"
+role = "Frontend Developer"
+experience = 2
+
+print("Name:", name)
+print("Role:", role)
+print("Experience:", experience, "years")
+
+if experience >= 2:
+    print("Eligible for project")
+else:
+    print("Need more experience")
